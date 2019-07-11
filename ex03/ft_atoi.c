82a3195c4e71ae957291d47f82bad2bfece9708c
Nathanael LABREUIL <alabreui@e2r11p16.42.fr>
2019-07-11 10:05:52 +0200

    Add ex03 and ex04 and Optimize ex00

--- ex03/ft_atoi.c
+++ ex03/ft_atoi.c
@@ -6,19 +6,40 @@
 /*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/10 12:17:36 by alabreui          #+#    #+#             */
-/*   Updated: 2019/07/10 12:29:13 by alabreui         ###   ########.fr       */
+/*   Updated: 2019/07/10 18:36:32 by alabreui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int is_white_space(char c)
+int		is_white_space(char c)
 {
-	if (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' || 
+	if (c == ' ' || c == '\t' || c == '\n' || c == '\v' || c == '\f' ||
 			(c == '\r'))
 		return (1);
-	return (0)
+	return (0);
 }
 
 int		ft_atoi(char *str)
 {
+	int result;
+	int minus_count;
+	int i;
 
+	i = 0;
+	minus_count = 0;
+	result = 0;
+	while (is_white_space(*str))
+		str++;
+	while (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			minus_count++;
+		str++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10;
+		result = result + (str[i] - '0');
+		i++;
+	}
+	return (minus_count % 2 != 0 ? -1 * result : result);
 }
---
+++ ex04/ft_putnbr_base.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/07/10 18:32:14 by alabreui          #+#    #+#             */
+/*   Updated: 2019/07/10 20:45:28 by alabreui         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int		is_not_alphanumeric(char c)
+{
+	if (!(c >= 'a' && c <= 'z') && !(c >= 'A' && c <= 'Z') &&
+			!(c >= '0' && c <= '9'))
+		return (1);
+	return (0);
+}
+
+int		check_base(char *base, int *base_length)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (base[i] != '\0')
+	{
+		if (is_not_alphanumeric(base[i]))
+			return (0);
+		j = i + 1;
+		while (base[j] != '\0')
+		{
+			if (base[i] == base[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	*base_length = i;
+	if (*base == '\0' || *base_length <= 1)
+		return (0);
+	return (1);
+}
+
+void	assign_digit(int nbr, char *base, int *base_length)
+{
+	if (nbr == -2147483648)
+	{
+		assign_digit(-214748364, base, base_length);
+		nbr = 8;
+	}
+	if (nbr < 0)
+	{
+		write(1, "-", 1);
+		nbr = nbr * -1;
+	}
+	if (nbr >= *base_length)
+	{
+		assign_digit(nbr / *base_length, base, base_length);
+		nbr = nbr % *base_length;
+	}
+	if (nbr < *base_length)
+	{
+		ft_putchar(base[nbr]);
+	}
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int length;
+	int *base_length;
+
+	length = 0;
+	base_length = &length;
+	if (check_base(base, base_length))
+		assign_digit(nbr, base, base_length);
+}
